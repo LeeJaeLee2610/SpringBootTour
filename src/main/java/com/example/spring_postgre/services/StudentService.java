7da4380f9f8543a9e58ae2b8c8861b2278937306
LeeJaeLee <cuonggaayf01@gmail.com>
2022-09-15 09:37:12 +0700

    Update 15/9/2022

--- src/main/java/com/example/spring_postgre/services/StudentService.java
+++ src/main/java/com/example/spring_postgre/services/StudentService.java
@@ -1,14 +1,20 @@
-package com.example.spring_postgre.database;
+package com.example.spring_postgre.service;
 
 import com.example.spring_postgre.model.Student;
 import com.example.spring_postgre.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class Database {
+public class Services {
     @Autowired
     private StudentRepository studentRepository;
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
 
     public Student getStudentBySid(Integer sid){
         return studentRepository.findStudentBySid(sid);
