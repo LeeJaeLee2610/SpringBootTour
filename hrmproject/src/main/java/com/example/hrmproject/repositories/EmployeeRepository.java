959e46a9b0eb9be85846fd80a8aec29fa0cf8ece
LeeJaeLee <cuonggaayf01@gmail.com>
2022-09-15 22:36:19 +0700

    Update 15/9/2022

---
+++ hrmproject/src/main/java/com/example/hrmproject/repositories/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.example.hrmproject.repositories;
+
+public class EmployeeRepository {
+    
+}
---
+++ hrmproject/src/main/java/com/example/hrmproject/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.hrmproject.repositories;
+
+public interface UserRepository {
+    
+}
